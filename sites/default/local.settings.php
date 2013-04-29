5141a518a2474582c8d8c2b191842b33dfc13e64
Sven Indelicato <sindelicato@redmouse.ch>
2013-04-29 16:35:48 +0200

    Setting files and files/ folder added"

---
+++ sites/default/local.settings.php
@@ -0,0 +1,18 @@
+<?php
+
+$databases = array (
+  'default' =>
+  array (
+    'default' =>
+    array (
+      'database' => 'newsmanager',
+      'username' => 'newsmanager',
+      'password' => 'r3dmous3NEW',
+      'host' => 'localhost',
+      'port' => '',
+      'driver' => 'mysql',
+      'prefix' => '',
+    ),
+  ),
+);
+
